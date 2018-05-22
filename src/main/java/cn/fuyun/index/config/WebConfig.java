419518a05494d9083dc2e25c674aef74c2ca7d7a
zirui <648390777@qq.com>
2018-05-22 11:24:04 +0800

    完成了登陆页面、配置了登陆检查拦截器

--- src/main/java/cn/fuyun/index/config/WebConfig.java
+++ src/main/java/cn/fuyun/index/config/WebConfig.java
@@ -1,8 +1,11 @@
 package cn.fuyun.index.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import cn.fuyun.index.interceptor.LoginInterceptor;
 
 /**
  * WebMvcµƒ“ª–©≈‰÷√
@@ -12,7 +15,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+	/**
+	 * ≈‰÷√æ≤Ã¨◊ ‘¥
+	 */
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+	}
+	
+	/**
+	 * ≈‰÷√¿πΩÿ∆˜
+	 */
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/**").excludePathPatterns("/login/**", "/static/**");
 	}
 }
--- src/main/java/cn/fuyun/index/controller/MenuController.java
+++ src/main/java/cn/fuyun/index/controller/MenuController.java
@@ -1,7 +1,14 @@
 package cn.fuyun.index.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import cn.fuyun.index.domain.User;
 
 /**
  * 主页的视图控制器
@@ -20,5 +27,26 @@ public class MenuController {
 	@RequestMapping("/")
 	public String home() {
 		return "home";
+	}
+	
+	/**
+	 *  解析到登陆页面
+	 * @return
+	 */
+	@RequestMapping(value = "/login",method = RequestMethod.GET)
+	public String login() {
+		return "login";
+	}
+	
+	/**
+	 *  解析到登陆页面
+	 * @return
+	 */
+	@RequestMapping(value = "/login",method = RequestMethod.POST)
+	public String checkLogin(User user, HttpServletRequest request, HttpServletResponse response) {
+		HttpSession session = request.getSession();
+		session.setAttribute("user", user);
+		System.out.println(user);
+		return "redirect:/";
 	}
 }
